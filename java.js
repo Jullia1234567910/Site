7ee7b90245c7b3d5d5245efc9eb28ec34d432693
Júllia <julliafagundesdasilva@gmail.com>
2025-07-05 23:29:13 -0300

    Delete java.js

--- java.js
+++
@@ -1,7 +0,0 @@
-function mostrarParagrafo(){
-var paragrafo = document.getElementById("meuParagrafo");
-if (paragrafo.style.display === "none") {
-    paragrafo.style.display = "block";}
-    else{
-        paragrafo.style.display = "none";}
-    }
