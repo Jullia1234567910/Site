ae669b01e5bdfad871ff7173fd50e157a2b8b9ec
Júllia <julliafagundesdasilva@gmail.com>
2025-07-05 23:25:25 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,7 @@
+function mostrarParagrafo(){
+var paragrafo = document.getElementById("meuParagrafo");
+if (paragrafo.style.display === "none") {
+    paragrafo.style.display = "block";}
+    else{
+        paragrafo.style.display = "none";}
+    }
